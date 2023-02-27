0d3f64c35cb5d24dcdeda22c5a2535a01e39b615
Andrea Occhipinti <andreantoniocchipinti@gmail.com>
2023-02-27 16:41:37 +0100

    Add cards

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 // import logo from './logo.svg';
+import React, {Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
+import Card from './components/Card'
+
+import pizza from './images/pizza.jpg'
+import cheescake from './images/cheescake.jpg'
+import cutlet from './images/cutlet.jpg'
+import ravioli from './images/ravioli.jpg'
+import orecchiette from './images/orecchiette.jpg'
+import spoonDessert from './images/spoonDessert.jpg'
 
 
-function App() {
-  return (
 
-    <div>
-      <Navbar />
-    </div>
+class App extends Component {
 
-  );
+  render() {
+    return (
+
+      <>
+        <Navbar />
+        <div className='container mb-3 mt-5'>
+          <h3>What would you like to order ?</h3>
+          <hr />
+          <div className='row'>
+            <Card image={pizza} name= 'Savory pie' price= '6,00' />
+            <Card image={cheescake} name= 'Strawberry cheesecake' price= '5,00'/>
+            <Card image={cutlet} name= 'Cutlet' price= '6,00'/>
+            <Card image={ravioli} name= 'Ravioli with butter and sage' price= '10,00'/>
+            <Card image={orecchiette} name= 'Orecchiette with turnip tops' price= '8,00'/>
+            <Card image={spoonDessert} name= 'Spoon dessert' price= '5,00'/>
+          </div>
+          {/* <div className='row'>
+            <
+  
+          </div> */}
+        </div>
+  
+      </>
+  
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+
+import React, {Component } from 'react';
+
+class Card extends Component {
+
+    render() {
+        return(
+            <div className="col mb-5 mt-4">
+                <div className="card" style={{width: '20rem', textAlign: 'center'}}>
+                    <img src={this.props.image} className="card-img-top" alt="plate" style={{height: '200px', width:'auto', objectFit:'cover' }}/>
+                    <div className="card-body">
+                        <h5 className="card-title">{this.props.name}</h5>
+                        <p className="card-text">€{this.props.price}</p>
+                        <button className="btn btn-outline-danger">Delete</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card
