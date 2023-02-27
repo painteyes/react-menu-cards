927f07afad9907775e24562622759ffc3f0172ec
Andrea Occhipinti <andreantoniocchipinti@gmail.com>
2023-02-27 12:03:48 +0100

    Create navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+
+function Navbar() {
+    return(
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container-fluid">
+            <span className="navbar-brand mb-0 h1">Forks'n'React</span>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
+
+
