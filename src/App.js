c65496713b0c0cd3f209ef61b2001bedf3056774
Andrea Occhipinti <andreantoniocchipinti@gmail.com>
2023-02-28 15:26:35 +0100

    Implemented card deletion functionality

--- src/App.js
+++ src/App.js
@@ -19,41 +19,73 @@ class App extends Component {
       {
         id: 0,
         name: 'Savory pie',
-        price: 6.00,
+        price: '6,00',
         image: savoryPie
       }, 
       {
         id: 1,
         name: 'Strawberry cheesecake',
-        price: 5.00,
+        price: '5,50',
         image: cheescake
       },
       {
         id: 2,
         name: 'Cutlet',
-        price: 7.00,
+        price: '7,00',
         image: cutlet
       },
       {
         id: 3,
         name: 'Ravioli with butter and sage',
-        price: 10.00,
+        price: '10,00',
         image: ravioli
       },
       {
         id: 4,
         name: 'Orecchiette with turnip tops',
-        price: 8.00,
+        price: '8,00',
         image: orecchiette
       },
       {
         id: 5,
         name: 'Spoon dessert',
-        price: 5.00,
+        price: '5,00',
         image: spoonDessert
       }
     ]
   }
+
+  // Gestisce l'eliminazione di una carta dall'elenco di carte memorizzate nello stato.
+  // Utilizza this.state.cards per creare una nuova lista di carte che non contiene la carta con l'id specificato.
+  // Aggiorna lo stato utilizzando this.setState.
+  handleDelete = cardId => {
+    const updatedCards = this.state.cards.filter(card => card.id !== cardId)
+    this.setState({cards: updatedCards})
+  }
+
+  // Gestisce l'eliminazione di una carta dall'elenco di carte memorizzate nello stato.
+  // Utilizza la lista filtrata per assegnare direttamente cards nella chiave dell'oggetto di stato.
+  // In questo modo, la chiamata a setState è più concisa.
+  _handleDelete = cardId => {
+    const cards = this.state.cards.filter(card => card.id !== cardId)
+    this.setState({cards})
+  }
+
+  // Gestisce l'eliminazione di una carta dall'elenco di carte memorizzate nello stato.
+  // Utilizza la funzione di callback prevState => ({}) di setState per accedere allo stato precedente e filtrare la lista di carte.
+  // La nuova lista viene quindi restituita come un oggetto che rappresenta lo stato aggiornato, e viene passata a setState.
+  // Questa forma di aggiornamento dello stato è più sicura, in quanto si utilizza il valore precedente dello stato per evitare di avere problemi con la concorrenza delle modifiche dello stato.
+  __handleDelete = cardId => {
+    this.setState(prevState => ({
+      cards: prevState.cards.filter(card => card.id !== cardId)
+    }));
+  }
+
+
+
+
+
+
   render() {
     return (
 
@@ -64,7 +96,7 @@ class App extends Component {
           <hr />
           <div className='row'>
             {this.state.cards.map(card => (
-              <Card key={card.id} image={card.image} name={card.name} price={card.price}/>
+              <Card key={card.id} card={card} onDelete={this.handleDelete} />
             ))}
           </div>
   
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,11 +7,28 @@ class Card extends Component {
         return(
             <div className="col mb-5 mt-4">
                 <div className="card" style={{width: '20rem', textAlign: 'center'}}>
-                    <img src={this.props.image} className="card-img-top" alt="plate" style={{height: '200px', width:'auto', objectFit:'cover' }}/>
+                    <img src={this.props.card.image} className="card-img-top" alt="plate" style={{height: '200px', width:'auto', objectFit:'cover' }}/>
                     <div className="card-body">
-                        <h5 className="card-title">{this.props.name}</h5>
-                        <p className="card-text">€{this.props.price}</p>
-                        <button className="btn btn-outline-danger">Delete</button>
+                        <h5 className="card-title">{this.props.card.name}</h5>
+                        <p className="card-text">{this.props.card.price} €</p>
+                        
+                        <button className="btn btn-outline-danger" onClick={()=>{this.props.onDelete(this.props.card.id)}}>Delete</button>
+                    {/*    
+                        Poiché la prop "onDelete" richiede un argomento, 
+                        la riga di codice "this.props.onDelete(this.props.card.id)" non è una funzione, ma una chiamata a funzione.
+
+                        Se passassimo la prop "onDelete" direttamente come funzione, 
+                        questa verrebbe eseguita durante il rendering iniziale del componente, 
+                        invece di essere eseguita solo quando il pulsante viene cliccato.
+
+                        Per evitare questo problema, invece di chiamare direttamente la prop "onDelete", 
+                        possiamo avvolgerla in una funzione arrow. 
+                        In questo modo, la chiamata alla prop "onDelete" verrà eseguita solo quando il pulsante viene cliccato.
+                    */}
+                    <button className="btn btn-outline-danger" onClick={() => this.props.onDelete(this.props.card.id)}>Delete</button>
+
+                    
+                    
                     </div>
                 </div>
             </div>
